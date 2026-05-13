ffa71674f2b7b9e46eb89ff407365c82f957721d
Dnelzre <daniel.zerbe2004@gmail.com>
2026-05-13 16:39:41 +0200

    Abgabe2-Fertig

--- test/TokenTest.java
+++ test/TokenTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TokenTest {
 
@@ -16,5 +17,13 @@ class TokenTest {
         m.append(ipr.ip(), ipr.port());
         String json_2 = m.toJSON();
         assertEquals(json_1, json_2);
+    }
+
+    @Test
+    void testRemoveEndpoint() {
+        Token token = new Token().append("ip0", 0).append("ip1", 1).append("ip2", 2);
+        assertTrue(token.removeEndpoint(new Token.Endpoint("ip1", 1)));
+        assertEquals(2, token.length());
+        assertEquals("ip0", token.first().ip());
     }
 }
